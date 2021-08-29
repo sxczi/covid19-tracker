de5e46cc7d923af32b0f2712d07b5c6515a764e7
uwuxia <uwuxia@uwuxias-MacBook-Air.local>
2021-08-29 07:54:24 +0300

    fixed deployment error

--- server.ts
+++ server.ts
@@ -7,7 +7,18 @@ import express from 'express';
 const app = express();
 
 async function getCovidData() {
-    const browser = await puppeteer.launch();
+    const chromeOptions = {
+        headless: true,
+        defaultViewport: null,
+        args: [
+            "--incognito",
+            "--no-sandbox",
+            "--single-process",
+            "--no-zygote"
+        ],
+    };
+
+    const browser = await puppeteer.launch(chromeOptions);
     const page = await browser.newPage();
 
     await page.goto('https://atlasgis.maps.arcgis.com/apps/opsdashboard/index.html#/e1105263db8541bcae0cb46dd4adc448', {
